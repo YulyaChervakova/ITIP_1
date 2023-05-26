7af1f8ec6875f755b3114409b72d08d2a2305b56
Yulya Dorofeeva <yulya@MacBook-Air-Ivan.local>
2023-05-26 15:11:53 +0300

    Last

--- mygroup.py
+++ mygroup.py
@@ -30,18 +30,29 @@ groupmates = [
         "marks": [3, 3, 4]
     }
 ]
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
 def print_students(students):
     error = -1
     while error == -1:
         print(u"Введите среднее значение")
         averageMark = input()
-        if averageMark.isnumeric() and 1 <= float(averageMark) and float(averageMark) <= 5:
+        if is_number(averageMark) and 1 <= float(averageMark) and float(averageMark) <= 5:
             error = 1
         else:
             print(u"Значение введено неверно")
     print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
     for student in students:
-        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
-    # int_number = int(input())
-    # print(int_number)
+        average = 0
+        for number in student["marks"]:
+            average += number
+        average = average/len(student["marks"])
+        if average > float(averageMark):
+            print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+
 print_students(groupmates)
